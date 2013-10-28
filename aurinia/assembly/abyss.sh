63f1cf90e6f64b3540643a9df68e7d5646b5ba43
Sasha Mikheyev <sasha@homologo.us>
2013-10-28 18:38:27 +0900

    tweaked assembly script to make it multi-threaded

--- aurinia/assembly/abyss.sh
+++ aurinia/assembly/abyss.sh
@@ -4,28 +4,28 @@
 #$ -j y
 ##$ -m ea
 #$ -cwd
-#$ -l mf=100G
-#$ -l h_vmem=100G
+#$ -l mf=150G
+#$ -l h_vmem=150G
 . $HOME/.bashrc
 export TEMPDIR=/genefs/MikheyevU/temp
 export TEMP=/genefs/MikheyevU/temp
 export TMP=/genefs/MikheyevU/temp
 export TMPDIR=/genefs/MikheyevU/temp 
 
+#check if name variable passed to script
 if [[ -z "$NAME" ]]; then
 	echo "NAME variable not set. Pass it using qsub -v."
 	exit 1
 fi
 
+#manage directories
 DIR=data/assemblies/k$SGE_TASK_ID
 if [ -d "$DIR" ]; then
 	rm -r data/assemblies/k$SGE_TASK_ID
 fi
 
 mkdir $DIR
-
 WD=`pwd`
-
 cd $DIR
 
-abyss-pe name=$NAME j=1 k=$SGE_TASK_ID in="$WD/data/trimmed/trimmed_1.fastq $WD/data/trimmed/trimmed_2.fastq" se="$WD/data/trimmed/trimmed_u.fastq"
+abyss-pe name=$NAME j=6 k=$SGE_TASK_ID in="$WD/data/trimmed/trimmed_1.fastq $WD/data/trimmed/trimmed_2.fastq" se="$WD/data/trimmed/trimmed_u.fastq"
